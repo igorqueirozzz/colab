b9d7dcc44c4ba1cfbb324389bc11cae4dbb9ecc3
Igor Queiroz <igorqueirozz@outlook.com>
2021-03-09 13:46:39 -0300

    #Adicionado visualizador de pdf na atividade de financeiros.

--- app/src/main/java/br/com/centralit/citcolab/activity/FinanceActivity.java
+++ app/src/main/java/br/com/centralit/citcolab/activity/FinanceActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 import br.com.centralit.citcolab.R;
 
 public class FinanceActivity extends AppCompatActivity {
+
+    private PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance);
+
+        pdfView = findViewById(R.id.pdfViewer);
+        pdfView.fromAsset("example.pdf")
+                .enableAntialiasing(true)
+                .load();
     }
 
     public void finishActivity(View view) {
